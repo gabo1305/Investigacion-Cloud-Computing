076d89fffb319bfdf6e6ac8049231f6cd047487d
Gabriel Solano Coronado <gabrielsolcor@estudiantec.cr>
2020-07-16 23:27:08 -0600

    Updated

---
+++ Apps/IBM1/hello.py
@@ -0,0 +1,89 @@
+from cloudant import Cloudant
+from flask import Flask, render_template, request, jsonify
+import atexit
+import os
+import json
+
+app = Flask(__name__, static_url_path='')
+
+db_name = 'mydb'
+client = None
+db = None
+
+if 'VCAP_SERVICES' in os.environ:
+    vcap = json.loads(os.getenv('VCAP_SERVICES'))
+    print('Found VCAP_SERVICES')
+    if 'cloudantNoSQLDB' in vcap:
+        creds = vcap['cloudantNoSQLDB'][0]['credentials']
+        user = creds['username']
+        password = creds['password']
+        url = 'https://' + creds['host']
+        client = Cloudant(user, password, url=url, connect=True)
+        db = client.create_database(db_name, throw_on_exists=False)
+elif "CLOUDANT_URL" in os.environ:
+    client = Cloudant(os.environ['CLOUDANT_USERNAME'], os.environ['CLOUDANT_PASSWORD'], url=os.environ['CLOUDANT_URL'], connect=True)
+    db = client.create_database(db_name, throw_on_exists=False)
+elif os.path.isfile('vcap-local.json'):
+    with open('vcap-local.json') as f:
+        vcap = json.load(f)
+        print('Found local VCAP_SERVICES')
+        creds = vcap['services']['cloudantNoSQLDB'][0]['credentials']
+        user = creds['username']
+        password = creds['password']
+        url = 'https://' + creds['host']
+        client = Cloudant(user, password, url=url, connect=True)
+        db = client.create_database(db_name, throw_on_exists=False)
+
+
+port = int(os.getenv('PORT', 8000))
+
+@app.route('/')
+def root():
+##    n=5
+##    factorial_total = 1
+##    while n > 1:
+##        factorial_total *= n
+##        n -= 1
+##    return str(factorial_total)
+
+    return app.send_static_file('index.html')
+
+
+@app.route('/api/visitors', methods=['GET'])
+def get_visitor():
+    if client:
+        return jsonify(list(map(lambda doc: doc['name'], db)))
+    else:
+        print('No database')
+        return jsonify([])
+
+
+@app.route('/api/visitors', methods=['POST'])
+def put_visitor():
+    user = request.json['name']
+    data = {'name':user}
+    if client:
+        my_document = db.create_document("data")
+        data['_id'] = my_document['_id']
+        return jsonify(data)
+    else:
+        print('No database')
+        return jsonify(data)
+@app.route('/')
+def factorial():
+    n=5
+    factorial_total = 1
+    while n > 1:
+        factorial_total *= n
+        n -= 1
+    return factorial_total
+
+
+
+@atexit.register
+def shutdown():
+    if client:
+        client.disconnect()
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=port, debug=True)
