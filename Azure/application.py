4afa799b4135f165bedff92d151fb0d67cd64fb1
Gabriel Solano Coronado <gabrielsolcor@estudiantec.cr>
2020-07-29 09:43:56 -0600

    Update application.py

--- Azure/application.py
+++ Azure/application.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask,render_template
 app = Flask(__name__)
+
 
 @app.route('/')
 def hello():
     return app.send_static_file('indexx.html')
 
 
-#if __name__ == '__main__':
- #   app.run(host='127.0.0.1', port=8080, debug=True)
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8080, debug=True)
